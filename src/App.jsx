7fb5d2548f43cc06137fddd0648aae0a900fdf9b
laevad <77134175+laevad@users.noreply.github.com>
2023-06-23 16:04:59 +0800

    create Products.jsx and add to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
+import {Products} from "./pages/Products.jsx";
+
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
         <div className="mt-6">
-            <button className="btn">Button</button>
+           <Products />
         </div>
     </>
   )
---
+++ src/pages/Products.jsx
@@ -0,0 +1,11 @@
+export const Products = () => {
+	return (
+		<>
+			<div className="max-w-4xl mx-auto">
+				<h1 className="text-2xl text-center my-5 font-bold underline">
+					Products
+				</h1>
+			</div>
+		</>
+	)
+}
