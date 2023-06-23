c0cf8346d940a63e69609e8b413bcccf5f5ef61b
laevad <77134175+laevad@users.noreply.github.com>
2023-06-23 15:55:21 +0800

    install daisy ui and test it

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ function App() {
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+        <div className="mt-6">
+            <button className="btn">Button</button>
+        </div>
     </>
   )
 }
