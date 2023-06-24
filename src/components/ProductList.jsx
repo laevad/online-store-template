8fd1d96a4a7359006fe2cbacad830be7c8d2445c
laevad <77134175+laevad@users.noreply.github.com>
2023-06-24 09:58:19 +0800

    redesign thee prod list

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,7 +2,7 @@
 export const ProductList = ({products}) => {
 	return (
 		<>
-			<div className="grid gap-3 md:grid-cols-3">
+			<div className="grid gap-4 md:grid-cols-3">
 				{/* eslint-disable-next-line react/prop-types */}
 				{products.map((product) => (
 					<div key={product.id} className="card w-full bg-base-100 shadow-xl">
@@ -11,15 +11,30 @@ export const ProductList = ({products}) => {
 						</figure>
 						<div className="card-body">
 							<h2 className="card-title">{product.title}</h2>
-							{/*	eclipses details */}
-							<p className="line-clamp-3">{product.description}</p>
-							{/*	view and add to card*/}
-							<div className="card-actions">
-								<button className="btn btn-primary">Add to cart</button>
-								<button className="btn btn-ghost">View</button>
+							<div className="justify-between card-actions">
+								<span className="badge badge-outline">
+									{/*peso sign*/}
+									&#8369;
+									{product.price}</span>
 							</div>
-							{/*category with tag icon*/}
-							<div className="badge badge-outline badge-accent">{product.category}</div>
+							{/*	preview details */}
+							<p className="text-sm">{product.description}</p>
+
+						</div>
+						<div className="card-footer mb-3">
+							{/*	view and add to card: spce between*/}
+							<div className="justify-between card-actions">
+								<button className="btn btn-outline btn-sm">
+									{/*view detail icon*/}
+									&#128269;
+									View
+								</button>
+								<button className="btn btn-primary btn-sm">
+									{/*	add to cart icon*/}
+									&#128722;
+									Add to Cart
+								</button>
+							</div>
 						</div>
 					</div>
 				))}
